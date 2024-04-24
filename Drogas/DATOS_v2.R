caa0e910e0270ac7606670c90e8418b3783afaae
luisa chaves pinzon <lchaves@unal.edu.co>
2024-04-23 21:59:25 -0500

    adicion de bibliografia, continuacion de limpieza

--- Drogas/DATOS_v2.R
+++ Drogas/DATOS_v2.R
@@ -5,7 +5,7 @@ library("tidyr")
 
 # CARGA Y LIMPIEZA DE LAS TABLAS ----
 
-## Básicos ----
+       ## Básicos ----
 personas <- read_csv("Datos originales/personas.csv")
 
 personas_seleccionadas <- read_csv("Datos originales/personas_seleccionadas.csv")
@@ -283,50 +283,299 @@ C_i <- C_i %>%
          I_07_B = case_when(!(I_07 == "1") ~ "na", is.na(I_07_B) ~ "2", TRUE ~ I_07_B),
          I_07_C = case_when(!(I_07 == "1") ~ "na", is.na(I_07_C) ~ "2", TRUE ~ I_07_C),
          I_07_D = case_when(!(I_07 == "1") ~ "na", is.na(I_07_D) ~ "2", TRUE ~ I_07_D),
-         I_07_E = case_when(!(I_07 == "1") ~ "na", is.na(I_07_E) ~ "2", TRUE ~ I_07_E))
+         I_07_E = case_when(!(I_07 == "1") ~ "na", is.na(I_07_E) ~ "2", TRUE ~ I_07_E)) %>%
+  mutate(I_05 = as.integer(I_05))
   
   
 colSums(is.na(C_i))
 
 #### J - Inhalables  ----
 table(C_g$G_11_C) #Todos (1)
-table(C_g$G_11_D) #Dick
-table(C_g$G_11_E) #Popper
+table(C_g$G_11_D) #Dick (2)
+table(C_g$G_11_E) #Popper (3)
 
 C_j <- read_csv("Datos originales/j_capitulos.csv")
+colSums(is.na(C_j))
 
 C_j_1 <- C_j %>%
-  select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`) %>%
+  select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`, -`J_07`, -`J_08`, -`J_09`, -`J_10`, -`J_11`, -`J_12`) %>%
   mutate_all(as.character) %>%
   left_join(C_g[, c("DIRECTORIO", "G_11_C")], by = c("DIRECTORIO" = "DIRECTORIO")) %>%
   filter(G_11_C == "1") %>%
-  mutate(J_01 == case_when(!(G_11_C == "1") ~ "na", TRUE ~ J_01),
-         J_02 == case_when(!(G_11_C == "1") ~ "na", TRUE ~ J_02),
-         J_03 == case_when((J_02 == "2") ~ "na", TRUE ~ J_03),
-         J_04 == case_when((J_02 == "2") ~ "na", TRUE ~ J_04),
+  mutate(J_03 = case_when(!(J_02 == "1") ~ "na", TRUE ~ J_03),
+         J_04 = case_when(!(J_02 == "1") ~ "na", TRUE ~ J_04),
          J_05_A = case_when(!(J_02 == "1") ~ "na", is.na(J_05_A) ~ "2", TRUE ~ J_05_A),
          J_05_B = case_when(!(J_02 == "1") ~ "na", is.na(J_05_B) ~ "2", TRUE ~ J_05_B),
          J_05_C = case_when(!(J_02 == "1") ~ "na", is.na(J_05_C) ~ "2", TRUE ~ J_05_C),
          J_05_D = case_when(!(J_02 == "1") ~ "na", is.na(J_05_D) ~ "2", TRUE ~ J_05_D),
          J_05_E = case_when(!(J_02 == "1") ~ "na", is.na(J_05_E) ~ "2", TRUE ~ J_05_E),
          J_05_F = case_when(!(J_02 == "1") ~ "na", is.na(J_05_F) ~ "2", TRUE ~ J_05_F),
-         J_05_A = case_when(!(J_02 == "1") ~ "na", is.na(J_06_A) ~ "2", TRUE ~ J_06_A),
+         J_06_A = case_when(!(J_02 == "1") ~ "na", is.na(J_06_A) ~ "2", TRUE ~ J_06_A),
          J_06_B = case_when(!(J_02 == "1") ~ "na", is.na(J_06_B) ~ "2", TRUE ~ J_06_B),
          J_06_C = case_when(!(J_02 == "1") ~ "na", is.na(J_06_C) ~ "2", TRUE ~ J_06_C),
          J_06_D = case_when(!(J_02 == "1") ~ "na", is.na(J_06_D) ~ "2", TRUE ~ J_06_D),
          J_06_E = case_when(!(J_02 == "1") ~ "na", is.na(J_06_E) ~ "2", TRUE ~ J_06_E),
          J_06_F = case_when(!(J_02 == "1") ~ "na", is.na(J_06_F) ~ "2", TRUE ~ J_06_F),
-         J_06_G = case_when(!(J_02 == "1") ~ "na", is.na(J_06_F) ~ "2", TRUE ~ J_06_G),
-         J_06_H = case_when(!(J_02 == "1") ~ "na", is.na(J_06_F) ~ "2", TRUE ~ J_06_H),
-         J_06_I = case_when(!(J_02 == "1") ~ "na", is.na(J_06_F) ~ "2", TRUE ~ J_06_I))
+         J_06_G = case_when(!(J_02 == "1") ~ "na", is.na(J_06_G) ~ "2", TRUE ~ J_06_G),
+         J_06_H = case_when(!(J_02 == "1") ~ "na", is.na(J_06_H) ~ "2", TRUE ~ J_06_H),
+         J_06_I = case_when(!(J_02 == "1") ~ "na", is.na(J_06_I) ~ "2", TRUE ~ J_06_I)) %>%
+  select(-`G_11_C`)
 colSums(is.na(C_j_1))
+
 C_j_2 <- C_j %>%
+  select(`J_07`, `J_08`, `J_09`, `J_10`, `DIRECTORIO`) %>%
+  mutate_all(as.character) %>%
+  left_join(C_g[, c("DIRECTORIO", "G_11_D")], by = c("DIRECTORIO" = "DIRECTORIO")) %>%
+  filter(G_11_D == "1") %>%
+  mutate(J_09 = case_when(!(J_08 == "1") ~ "na", TRUE ~ J_09),
+         J_10 = case_when(!(J_08 == "1") ~ "na", TRUE ~ J_10)) %>%
+  select(-`G_11_D`)
+
+colSums(is.na(C_j_2))
+
+C_j_3 <- C_j %>%
+  select(`J_11`, `J_12`, `DIRECTORIO`) %>%
+  mutate_all(as.character) %>%
+  left_join(C_g[, c("DIRECTORIO", "G_11_E")], by = c("DIRECTORIO" = "DIRECTORIO")) %>%
+  filter(G_11_E == "1") %>%
+  mutate(J_12 = case_when(!(J_11 == "1") ~ "na", TRUE ~ J_12)) %>%
+  select(-`G_11_E`)
+
+colSums(is.na(C_j_3))
+
+
+#### K - Marihuana  ----
+table(C_g$G_11_F)
+
+C_k <- read_csv("Datos originales/k_capitulos.csv")
+C_k <- C_k %>%
   select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`) %>%
   mutate_all(as.character) %>%
-  left_join(C_g[, c("DIRECTORIO", "G_11_D")], by = c("DIRECTORIO" = "DIRECTORIO"))
+  # Solo se marca cuando respondieron "1":si se pone "2" para no
+  mutate(K_01_A = replace_na(K_01_A, "2"),
+         K_01_B = replace_na(K_01_B, "2"),
+         K_01_C = replace_na(K_01_C, "2"),
+         K_01_D = replace_na(K_01_D, "2"),
+         K_04 = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_04),
+         K_05 = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_05),
+         K_06 = case_when(!(K_05 == "1") ~ "na", TRUE ~ K_06),
+         K_07 = case_when(!(K_05 == "1") ~ "na", TRUE ~ K_07),
+         K_08 = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_08),
+         K_09 = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_09),
+         K_09_VALOR = case_when(!(K_09 == "1") ~ "na", TRUE ~ K_09_VALOR),
+         K_09_TEXTO = case_when(!(K_09 == "1") ~ "na", TRUE ~ K_09_TEXTO),
+         K_10_A = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_A),
+         K_10_B = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_B),
+         K_10_C = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_C),
+         K_10_D = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_D),
+         K_10_E = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_E),
+         K_10_F = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_F),
+         K_10_G = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_G),
+         K_10_H = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_H),
+         K_10_I = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_10_I),
+         K_10_A = replace_na(K_10_A, "2"),
+         K_10_B = replace_na(K_10_B, "2"),
+         K_10_C = replace_na(K_10_C, "2"),
+         K_10_D = replace_na(K_10_D, "2"),
+         K_10_E = replace_na(K_10_E, "2"),
+         K_10_F = replace_na(K_10_F, "2"),
+         K_10_G = replace_na(K_10_G, "2"),
+         K_10_H = replace_na(K_10_H, "2"),
+         K_10_I = replace_na(K_10_I, "2"),
+         K_11 = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_11),
+         K_11_A = case_when(!(K_11 == "1") ~ "na", TRUE ~ K_11_A),
+         K_11_B = case_when(!(K_11 == "1") ~ "na", TRUE ~ K_11_B),
+         K_11_C = case_when(!(K_11 == "1") ~ "na", TRUE ~ K_11_C),
+         K_11_D = case_when(!(K_11 == "1") ~ "na", TRUE ~ K_11_D),
+         K_11_E = case_when(!(K_11 == "1") ~ "na", TRUE ~ K_11_E),
+         K_11_A = replace_na(K_11_A, "2"),
+         K_11_B = replace_na(K_11_B, "2"),
+         K_11_C = replace_na(K_11_C, "2"),
+         K_11_D = replace_na(K_11_D, "2"),
+         K_11_E = replace_na(K_11_E, "2"),
+         K_12_A = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_A),
+         K_12_B = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_B),
+         K_12_C = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_C),
+         K_12_D = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_D),
+         K_12_E = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_E),
+         K_12_F = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_F),
+         K_12_G = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_G),
+         K_12_H = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_H),
+         K_12_I = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_I),
+         K_12_J = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_J),
+         K_12_K = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_K),
+         K_12_L = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_L),
+         K_12_M = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_M),
+         K_12_N = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_N),
+         K_12_O = case_when(!(K_03 == "1") ~ "na", TRUE ~ K_12_O))
+    
+colSums(is.na(C_k))
 
+#### L - Cocaína  ----
+table(C_g$G_11_G)
 
+C_l <- read_csv("Datos originales/l_capitulos.csv")
+C_l <- C_l %>%
+  select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`) %>%
+  mutate_all(as.character) %>%
+  mutate(L_03 = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_03),
+         L_04 = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_04),
+         L_05 = case_when(!(L_04 == "1") ~ "na", TRUE ~ L_05),
+         L_06 = case_when(!(L_04 == "1") ~ "na", TRUE ~ L_06),
+         L_07 = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_07),
+         L_08 = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_08),
+         L_08_VALOR = case_when(!(L_08 == "1") ~ "na", TRUE ~ L_08_VALOR),
+         L_08_TEXTO = case_when(!(L_08 == "1") ~ "na", TRUE ~ L_08_TEXTO),
+         L_08 = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_08),
+         L_09_A = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_A),
+         L_09_B = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_B),
+         L_09_C = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_C),
+         L_09_D = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_D),
+         L_09_E = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_E),
+         L_09_F = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_F),
+         L_09_G = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_G),
+         L_09_H = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_H),
+         L_09_I = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_09_I),
+         L_09_A = replace_na(L_09_A, "2"),
+         L_09_B = replace_na(L_09_B, "2"),
+         L_09_C = replace_na(L_09_C, "2"),
+         L_09_D = replace_na(L_09_D, "2"),
+         L_09_E = replace_na(L_09_E, "2"),
+         L_09_F = replace_na(L_09_F, "2"),
+         L_09_G = replace_na(L_09_G, "2"),
+         L_09_H = replace_na(L_09_H, "2"),
+         L_09_I = replace_na(L_09_I, "2"),
+         L_10 = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_10),
+         L_10_A = case_when(!(L_10 == "1") ~ "na", TRUE ~ L_10_A),
+         L_10_B = case_when(!(L_10 == "1") ~ "na", TRUE ~ L_10_B),
+         L_10_A = replace_na(L_10_A, "2"),
+         L_10_B = replace_na(L_10_B, "2"),
+         L_11_A = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_A),
+         L_11_B = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_B),
+         L_11_C = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_C),
+         L_11_D = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_D),
+         L_11_E = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_E),
+         L_11_F = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_F),
+         L_11_G = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_G),
+         L_11_H = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_H),
+         L_11_I = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_I),
+         L_11_J = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_J),
+         L_11_K = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_K),
+         L_11_L = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_L),
+         L_11_M = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_M),
+         L_11_N = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_N),
+         L_11_O = case_when(!(L_02 == "1") ~ "na", TRUE ~ L_11_O))
 
+colSums(is.na(C_l))
+
+#### M - Basuco  ----
+table(C_g$G_11_H)
+
+C_m <- read_csv("Datos originales/m_capitulos.csv")
+C_m <- C_m %>%
+  select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`) %>%
+  mutate_all(as.character) %>%
+  mutate(M_03 = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_03),
+         M_04 = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_04),
+         M_05 = case_when(!(M_04 == "1") ~ "na", TRUE ~ M_05),
+         M_06 = case_when(!(M_04 == "1") ~ "na", TRUE ~ M_06),
+         M_07 = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_07),
+         M_08 = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_08),
+         M_08_VALOR = case_when(!(M_08 == "1") ~ "na", TRUE ~ M_08_VALOR),
+         M_08_TEXTO = case_when(!(M_08 == "1") ~ "na", TRUE ~ M_08_TEXTO),
+         M_09_A = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_A),
+         M_09_B = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_B),
+         M_09_C = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_C),
+         M_09_D = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_D),
+         M_09_E = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_E),
+         M_09_F = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_F),
+         M_09_G = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_G),
+         M_09_H = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_H),
+         M_09_I = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_09_I),
+         M_09_A = replace_na(M_09_A, "2"),
+         M_09_B = replace_na(M_09_B, "2"),
+         M_09_C = replace_na(M_09_C, "2"),
+         M_09_D = replace_na(M_09_D, "2"),
+         M_09_E = replace_na(M_09_E, "2"),
+         M_09_F = replace_na(M_09_F, "2"),
+         M_09_G = replace_na(M_09_G, "2"),
+         M_09_H = replace_na(M_09_H, "2"),
+         M_09_I = replace_na(M_09_I, "2"),
+         M_10_A = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_A),
+         M_10_B = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_B),
+         M_10_C = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_C),
+         M_10_D = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_D),
+         M_10_E = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_E),
+         M_10_F = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_F),
+         M_10_G = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_G),
+         M_10_H = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_H),
+         M_10_I = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_I),
+         M_10_J = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_J),
+         M_10_K = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_K),
+         M_10_L = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_L),
+         M_10_M = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_M),
+         M_10_N = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_N),
+         M_10_O = case_when(!(M_02 == "1") ~ "na", TRUE ~ M_10_O))
+
+colSums(is.na(C_m))
+
+#### N - Éxtasis  ----
+table(C_g$G_11_I)
+
+C_n <- read_csv("Datos originales/n_capitulos.csv")
+C_n <- C_n %>%
+  select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`) %>%
+  mutate_all(as.character) %>%
+  mutate(N_03 = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_03),
+         N_04 = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_04),
+         N_05 = case_when(!(N_04 == "1") ~ "na", TRUE ~ N_05),
+         N_06_A = case_when(!(N_04 == "1") ~ "na", TRUE ~ N_06_A),
+         N_06_B = case_when(!(N_04 == "1") ~ "na", TRUE ~ N_06_B),
+         N_06_C = case_when(!(N_04 == "1") ~ "na", TRUE ~ N_06_C),
+         N_06_A = replace_na(N_06_A, "2"),
+         N_06_B = replace_na(N_06_B, "2"),
+         N_06_C = replace_na(N_06_C, "2"),
+         N_07 = case_when(!(N_06_A == "1") ~ "na", TRUE ~ N_07),
+         N_08 = case_when(!(N_04 == "1") ~ "na", TRUE ~ N_08),
+         N_09 = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_09),
+         N_09_TEXTO = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_09_TEXTO),
+         N_10_A = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_A),
+         N_10_B = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_B),
+         N_10_C = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_C),
+         N_10_D = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_D),
+         N_10_E = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_E),
+         N_10_F = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_F),
+         N_10_G = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_G),
+         N_10_H = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_H),
+         N_10_I = case_when(!(N_02 == "1") ~ "na", TRUE ~ N_10_I),
+         N_10_A = replace_na(N_10_A, "2"),
+         N_10_B = replace_na(N_10_B, "2"),
+         N_10_C = replace_na(N_10_C, "2"),
+         N_10_D = replace_na(N_10_D, "2"),
+         N_10_E = replace_na(N_10_E, "2"),
+         N_10_F = replace_na(N_10_F, "2"),
+         N_10_G = replace_na(N_10_G, "2"),
+         N_10_H = replace_na(N_10_H, "2"),
+         N_10_I = replace_na(N_10_I, "2"),)
+
+colSums(is.na(C_n))
+
+#### O - Heroína  ----
+table(C_g$G_11_J)
+
+C_o <- read_csv("Datos originales/o_capitulos.csv")
+colSums(is.na(C_o))
+
+C_oa <- C_o %>%
+  select(-`SECUENCIA_ENCUESTA`, -`SECUENCIA_P`, -`ORDEN`) %>%
+  mutate_all(as.character) %>%
+  mutate(O_03 = case_when(!(O_02 == "1") ~ "na", TRUE ~ O_03),
+         O_04_A = case_when(!(O_02 == "1") ~ "na", TRUE ~ O_04_A),
+         O_04_B = case_when(!(O_02 == "1") ~ "na", TRUE ~ O_04_B),
+         O_04_C = case_when(!(O_02 == "1") ~ "na", TRUE ~ O_04_C),
+         O_04_D = case_when(!(O_02 == "1") ~ "na", TRUE ~ O_04_D),
+         O_04_FUMADA = case_when(!(O_04_A == "1") ~ "na", TRUE ~ O_04_FUMADA), )
+  
 # ---------------------------------------------------------------------------- #
 # ---------------------------------------------------------------------------- #
 
